e86b2e0c6238eaa5aee38b11b24a13ceba4214f7
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2025-03-17 12:58:31 +0900

    CI 구축

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: pluginPrettier
+    },
+    rules: {
+      "prettier/prettier": "error"
+    }
+  }
+];
---
+++ src/test/unit/sub.test.js
@@ -0,0 +1,5 @@
+import {sub} from '../../../src/functions/sub';
+
+test('sub 2 - 1 to equal 1', () => {
+    expect(sub(2, 1)).toBe(1);
+});
