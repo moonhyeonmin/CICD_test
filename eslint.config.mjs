56090ce0c845d2cc02d939c0bf339c71d935ea33
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2025-03-23 11:05:52 +0900

    prettier style 일치

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,24 @@
-// eslint.config.mjs
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-import pluginPrettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
-import pluginJest from "eslint-plugin-jest";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import pluginJest from 'eslint-plugin-jest';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+
+  // 명시적으로 글로벌 변수를 readonly로 설정
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: {
       globals: {
-        ...globals.node,
-        ...globals.es2021,
-        process: "readonly",
-        module: "readonly",
-        require: "readonly",
-        __dirname: "readonly",
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        global: 'readonly',
+        ...globals.es2021, // 최신 ES 글로벌 변수 적용
       },
     },
   },
@@ -31,23 +32,25 @@ export default [
       prettier: pluginPrettier,
     },
     rules: {
-      "prettier/prettier": "error",
+      'prettier/prettier': 'error',
     },
   },
 
+  // Jest 설정 추가
   {
-    files: ["**/*.test.js", "**/*.spec.js", "**/__tests__/**/*.js"],
+    files: ['**/*.test.js', '**/*.spec.js', '**/__tests__/**/*.js'],
     plugins: {
       jest: pluginJest,
     },
     languageOptions: {
       globals: {
-        describe: "readonly",
-        test: "readonly",
-        expect: "readonly",
-        beforeEach: "readonly",
-        afterEach: "readonly",
-        jest: "readonly",
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        global: 'readonly',
+        ...globals.es2021, // 최신 ES 글로벌 변수 적용
+        ...pluginJest.environments.globals,
       },
     },
     rules: {
