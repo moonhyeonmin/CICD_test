ffab281146d3735d98b493f34629aa71479fc8d7
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2025-03-23 11:22:52 +0900

    jest

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {add} from './functions/add';
-import {sub} from './functions/sub';
+import {add} from './functions/add.js';
+import {sub} from './functions/sub.js';
 
 export const main = (a, b) => {
     return add(a, b) + sub(a, b);
